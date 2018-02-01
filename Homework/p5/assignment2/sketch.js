039671aa983b91a3feaf7703c4ca75c64977e17c
Nikki Wong <nikkiwong@miami.edu>
2018-01-31 19:12:07 -0500

    testing

--- Homework/p5/assignment2/sketch.js
+++ Homework/p5/assignment2/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   // put setup code here
+  createCanvas(500,500);
+  background(255);
 }
 
 function draw() {
   // put drawing code here
+  fill("red");
+  rect(centerX -150,centerY -100,100,100);
+  ellipse(centerX -100,centerY -50,50,50);
+  rect(centerX +25,centerY -100,100,100);
+  ellipse(centerX +75,centerY -50,50,75);
 }
