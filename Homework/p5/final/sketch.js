cc981333f24038cfe1c8d36c8ebb439b77de6ef2
Nikki Wong <nikkiwong@miami.edu>
2018-04-19 12:48:40 -0400

    12

--- Homework/p5/final/sketch.js
+++ Homework/p5/final/sketch.js
@@ -4,4 +4,21 @@ function setup() {
 
 function draw() {
   // put drawing code here
+  createCanvas(1000,600);
+  background(255);
+  stroke(0);
+  rect(10,0,100,400);
+  rect(110,0,100,400);
+  rect(210,0,100,400);
+  rect(310,0,100,400);
+  rect(410,0,100,400);
+  rect(510,0,100,400);
+  rect(610,0,100,400);
+  rect(710,0,100,400);
+  fill(0);
+  rect(90,0,40,230);
+  rect(190,0,40,230);
+  rect(390,0,40,230);
+  rect(490,0,40,230);
+  rect(590,0,40,230);
 }
