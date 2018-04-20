d6f0fed0173a05dcea1451d33797f5afeaf21261
Nikki Wong <nikkiwong@miami.edu>
2018-04-20 13:41:54 -0400

    Final update

--- Homework/p5/final/sketch.js
+++ Homework/p5/final/sketch.js
@@ -7,7 +7,12 @@ var value5 = 255;
 var value6 = 255;
 var value7 = 255;
 var value8 = 255;
-var notes = [60,62, 64, 65, 67, 69, 71, 72];
+var value9 = 0;
+var value10 = 0;
+var value11 = 0;
+var value12 = 0;
+var value13 = 0;
+var notes = [60,61,62, 63,64, 65, 66, 67, 68, 69, 70, 71, 72];
 var trigger = 0;
 
 function setup() {
@@ -22,13 +27,13 @@ function setup() {
 function playNote(note, duration) {
   osc.freq(midiToFreq(note));
   // Fade it in
-  osc.fade(0.5,0.2);
+  osc.fade(0.2,0.2);
 
   // If we set a duration, fade it out
   if (duration) {
     setTimeout(function() {
       osc.fade(0,0.2);
-    }, duration-50);
+    }, duration-40);
   }
 }
 
@@ -40,37 +45,49 @@ function draw() {
 
   textSize(16);
   fill(200);
-  text("a",50,430);
-  text("s",150,430);
-  text("d",250,430);
-  text("f",350,430);
-  text("j",450,430);
-  text("k",550,430);
-  text("l",650,430);
-  text(";",750,430);
+  text("a",50,460);
+  text("s",150,460);
+  text("d",250,460);
+  text("f",350,460);
+  text("j",450,460);
+  text("k",550,460);
+  text("l",650,460);
+  text(";",750,460);
+
+  textSize(16);
+  text("q", 110,10);
+  text("e", 210,10);
+  text("r", 410,10);
+  text("u", 510,10);
+  text("i", 610,10);
 
   fill(value8);
-  rect(10,0,100,400);
+  rect(10,30,100,400);
   fill(value7);
-  rect(110,0,100,400);
+  rect(110,30,100,400);
   fill(value6);
-  rect(210,0,100,400);
+  rect(210,30,100,400);
   fill(value5);
-  rect(310,0,100,400);
+  rect(310,30,100,400);
   fill(value4);
-  rect(410,0,100,400);
+  rect(410,30,100,400);
   fill(value3);
-  rect(510,0,100,400);
+  rect(510,30,100,400);
   fill(value2);
-  rect(610,0,100,400);
+  rect(610,30,100,400);
   fill(value1);
-  rect(710,0,100,400);
-  fill(0);
-  rect(90,0,40,230);
-  rect(190,0,40,230);
-  rect(390,0,40,230);
-  rect(490,0,40,230);
-  rect(590,0,40,230);
+  rect(710,30,100,400);
+
+  fill(value11);
+  rect(90,30,40,230);
+  fill(value12);
+  rect(190,30,40,230);
+  fill(value9);
+  rect(390,30,40,230);
+  fill(value10);
+  rect(490,30,40,230);
+  fill(value13);
+  rect(590,30,40,230);
 
 }
 
@@ -79,36 +96,62 @@ function keyTyped() {
 
   if (key === ';') {
     value1 = "#ffcce6";
-    playNote(notes[7]);
+    playNote(notes[12]);
   }
 
   if (key === 'l') {
     value2 = "#ff4da6";
-    playNote(notes[6]);
+    playNote(notes[11]);
+  }
+
+  if (key === 'i') {
+    value13 = 100;
+    playNote(notes[10]);
   }
 
   if (key === 'k') {
     value3 = "#c653c6";
-    playNote(notes[5]);
+    playNote(notes[9]);
+  }
+
+  if (key === 'u') {
+    value10 = 100;
+    playNote(notes[8]);
   }
 
   if (key === 'j') {
     value4 = "#1a75ff";
-    playNote(notes[4]);
+    playNote(notes[7]);
+  }
+
+  if (key === 'r') {
+    value9 = 100;
+    playNote(notes[6]);
   }
 
   if (key === 'f') {
     value5 = "#47d147";
-    playNote(notes[3]);
+    playNote(notes[5]);
   }
 
   if (key === 'd') {
     value6 = "#ffd11a";
-    playNote(notes[2]);
+    playNote(notes[4]);
+  }
+
+  if (key === 'e') {
+    value12 = 100;
+    playNote(notes[3]);
   }
 
   if (key === 's') {
     value7 = "#ff751a";
+    playNote(notes[2]);
+  }
+
+
+  if (key === 'q') {
+    value11 = 100;
     playNote(notes[1]);
   }
 
@@ -170,6 +213,36 @@ function keyReleased() {
     value8 = 255;
   } else {
     value8 = 255;
+  }
+
+  if (value9 === 0) {
+    value9 = 0;
+  } else {
+    value9 = 0;
+  }
+
+  if (value10 === 0) {
+    value10 = 0;
+  } else {
+    value10 = 0;
+  }
+
+  if (value11 === 0) {
+    value11 = 0;
+  } else {
+    value11 = 0;
+  }
+
+  if (value12 === 0) {
+    value12 = 0;
+  } else {
+    value12 = 0;
+  }
+
+  if (value13 === 0) {
+    value13 = 0;
+  } else {
+    value13 = 0;
   }
 
   return false; // prevent any default behavior
